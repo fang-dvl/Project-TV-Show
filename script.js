c50bb78feae12f251bab32afd5470b7f1ff67690
Fanglai <laura@FLMacBook-Air.local>
2026-02-03 17:44:29 +0100

    finished level 100

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 //You can edit ALL of the code here
+import { getAllEpisodes } from "./episodes.js";
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
 }
-
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  rootElem.innerHTML = '';
+  for (let i = 0; i <episodeList.length; i++)
+     {const episodeContainer = document.createElement('section');
+      episodeContainer.classList.add('episode'); 
+      const title=document.createElement('h3');
+      const name=episodeList[i].name;
+      const season=String(episodeList[i].season).padStart(2, '0');
+      const number=String(episodeList[i].number).padStart(2, '0');
+      title.textContent=`${name}-S${season}E${number}`;
+      title.classList.add('title'); 
+      const image=document.createElement('img');
+      image.src=episodeList[i].image.medium
+      const summary=document.createElement('p');
+      summary.innerHTML = episodeList[i].summary || '';
+      summary.classList.add('summary'); 
+      episodeContainer.append(title,image,summary);
+      rootElem.append(episodeContainer);
+     }
 }
 
 window.onload = setup;
